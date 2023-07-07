b3381f402d6285b01aec89992855ca92e8d574f8
Михаил Ренжин <renzhin@gmail.com>
2023-07-07 14:46:53 +0300

    добавление моделей и локализации для приложения ice_cream

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,11 @@
+# ice_cream/admin.py
 from django.contrib import admin
 
-# Register your models here.
+# Из модуля models импортируем модель Category...
+from .models import Category, Topping, Wrapper, IceCream
+
+# ...и регистрируем её в админке:
+admin.site.register(Category)
+admin.site.register(Topping)
+admin.site.register(Wrapper)
+admin.site.register(IceCream)
