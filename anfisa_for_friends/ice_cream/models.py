f2ae5d02318ff7ec34ebd334570fa397397423f9
Михаил Ренжин <renzhin@gmail.com>
2023-07-12 15:23:02 +0300

    Добавление новых шаблонов, написание view-функций с обращениями к БД

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -49,6 +49,11 @@ class Wrapper(PublishedModel):
 class IceCream(PublishedModel):
     title = models.CharField(max_length=256, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     wrapper = models.OneToOneField(
         Wrapper,
         on_delete=models.SET_NULL,
@@ -69,6 +74,11 @@ class IceCream(PublishedModel):
     class Meta:
         verbose_name = 'мороженое'
         verbose_name_plural = 'Мороженое'
+
+        # Сначала сортируем по полю output_order,
+        # а если у нескольких объектов значения output_order совпадают--
+        # сортируем по title.
+        ordering = ('output_order', 'title')
 
     def __str__(self):
         return self.title
